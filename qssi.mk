c3753b584bdd15bb972e30bcbc61816bdd972364
Shashi Shekar Shankar <ssbang@codeaurora.org>
2019-01-11 13:33:22 +0530

    perf: QSSI/ QCV changes
    1. Move IOPHAL compile time enablement to runtime.
    2. Move LM compile time enablement to runtime.
    3. Move properties to vendor config store.
    
    Change-Id: If2c141aeed8b84a57dde11e8e765885a60397e56

--- qssi.mk
+++ qssi.mk
@@ -234,10 +234,6 @@ ifeq ($(ENABLE_VENDOR_IMAGE), true)
  VENDOR_SECURITY_PATCH := 2018-06-05
 endif
 
-#Property to enable/disable scroll pre-obtain view
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.scroll.preobtain.enable := false
-
 TARGET_USES_MKE2FS := true
 
 PRODUCT_PROPERTY_OVERRIDES += \
