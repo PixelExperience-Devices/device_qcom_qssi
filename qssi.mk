e282c83696100c589e939bcc8123764d1011fe6f
Naseer Ahmed <naseer@codeaurora.org>
2019-01-25 18:40:25 -0500

    qssi: Remove display includes
    Change-Id: Ib091766691fdccb752b8837218853309dfa0067c

--- qssi.mk
+++ qssi.mk
@@ -79,9 +79,6 @@ BOARD_FRP_PARTITION_NAME := frp
 #Android EGL implementation
 PRODUCT_PACKAGES += libGLES_android
 
--include hardware/qcom/display/config/msmnile.mk
-
-
 PRODUCT_BOOT_JARS += tcmiface
 PRODUCT_BOOT_JARS += telephony-ext
 PRODUCT_PACKAGES += telephony-ext
