d451b76ea4bfbccc18784e71a089258b109e2d63
padarshr <padarshr@codeaurora.org>
2019-01-30 18:26:48 +0530

    QSSI: Add audio configs path.
    Add vendor/qcom/opensource/audio-hal path as well for audio
    configs pickup.
    
    Change-Id: I215c2c7b0566534f7893acb9c0bbac781b457be4

--- qssi.mk
+++ qssi.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # Audio configuration file
 -include $(TOPDIR)hardware/qcom/audio/configs/msmnile/msmnile.mk
+-include $(TOPDIR)vendor/qcom/opensource/audio-hal/primary-hal/configs/msmnile/msmnile.mk
 
 #Audio DLKM
 AUDIO_DLKM := audio_apr.ko
