52bd2696f0cdd45dadc9e1b118abc04dbb4d2a51
padarshr <padarshr@codeaurora.org>
2019-02-04 19:13:54 +0530

    Qssi: Remove BOARD_USES_RECOVERY_AS_BOOT setting.
    Remove recovery setting in Qssi, since we don't need
    recovery ramdisk/content in Qssi.
    Due to this un-cleaned setting, OTA script errors out
    expecting recovery/fstab to be present, so this cleanly
    removes all of recovery partition settings now.
    
    Change-Id: I6726b2192eefd36c705e3e07c2da51dd0e535640

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,6 @@ AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS ?= boot system
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 TARGET_NO_RECOVERY := true
-BOARD_USES_RECOVERY_AS_BOOT := true
 else
 TARGET_NO_RECOVERY := true
 # Enable System As Root even for non-A/B
