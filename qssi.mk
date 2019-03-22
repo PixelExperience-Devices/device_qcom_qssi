4e022e62f797ec80a33cf2536e364d68c5f564f0
Divya Sharma <divyash@codeaurora.org>
2019-03-21 17:45:23 -0700

    Add missing PRODUCT_HOST_PACKAGES
    Change-Id: I6bd1242f92ecae476932596a66263539056231d2

--- qssi.mk
+++ qssi.mk
@@ -153,6 +153,10 @@ PRODUCT_PACKAGES += update_engine \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
+PRODUCT_HOST_PACKAGES += \
+    brillo_update_payload \
+    fips.py
+
 #Boot control HAL test app
 PRODUCT_PACKAGES_DEBUG += bootctl
 endif
