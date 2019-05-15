97aa2720b15c1da171b5a4a38614d7d637ef8feb
padarshr <padarshr@codeaurora.org>
2019-05-15 11:23:40 +0530

    Add non-a/b ota configs with dynamic partitions enabled.
    Change-Id: I22c42fefaf082ec3bd9c0a8628912cfd014ca203

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -86,9 +86,16 @@ ifeq ($(ENABLE_AB), true)
         $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_item_list)
     endif
 else
-$(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
-$(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
-$(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+    # Handle Case for QSSI-Dynamic Partition
+    ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
+        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+    else
+        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
+        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
+        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
+    endif
 endif
 
 #----------------------------------------------------------------------
