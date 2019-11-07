79aa7566ab7e9850ee72f9fd9a2e2a1bd79cf37f
padarshr <padarshr@codeaurora.org>
2019-11-07 13:54:34 +0530

    Extracts QSSI merge config logic to a new MergeConfig.mk file.
    This file can then be included from other targets.
    
    Change-Id: I99cbfe48ed3c1ce712f900fb930221f2244c48c5

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -78,31 +78,6 @@ LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 LOCAL_MODULE_PATH  := $(TARGET_OUT_KEYLAYOUT)
 include $(BUILD_PREBUILT)
 
-# QSSI merge config files
-ifeq ($(ENABLE_AB), true)
-    # Handle Case for QSSI-Dynamic Partition
-    ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/ab/merge_config_system_misc_info_keys)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/ab/merge_config_system_item_list)
-    else
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_misc_info_keys)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/ab/merge_config_system_item_list)
-    endif
-else
-    # Handle Case for QSSI-Dynamic Partition
-    ifeq ($(BOARD_DYNAMIC_PARTITION_ENABLE), true)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_item_list)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/non_ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/dynamic_partition/non_ab/merge_config_system_misc_info_keys)
-    else
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_item_list)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_other_item_list)
-        $(call dist-for-goals,droidcore,$(LOCAL_PATH)/ota_merge_configs/without_dynamic_partition/non_ab/merge_config_system_misc_info_keys)
-    endif
-endif
-
 #----------------------------------------------------------------------
 # Configs common to AndroidBoard.mk for all targets
 #----------------------------------------------------------------------
