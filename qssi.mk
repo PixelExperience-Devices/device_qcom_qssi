9c50abdeb87013e32bfe224001339ddb1b267f12
YSR <suresh@codeaurora.org>
2021-02-22 13:37:48 -0800

    Fix tab spaces to avoid warnings

--- qssi.mk
+++ qssi.mk
@@ -88,11 +88,11 @@ $(call inherit-product, device/qcom/qssi/common64.mk)
 
 #Inherit all except heap growth limit from phone-xhdpi-2048-dalvik-heap.mk
 PRODUCT_PROPERTY_OVERRIDES  += \
-	dalvik.vm.heapstartsize=8m \
-	dalvik.vm.heapsize=512m \
-	dalvik.vm.heaptargetutilization=0.75 \
-	dalvik.vm.heapminfree=512k \
-	dalvik.vm.heapmaxfree=8m
+     dalvik.vm.heapstartsize=8m \
+     dalvik.vm.heapsize=512m \
+     dalvik.vm.heaptargetutilization=0.75 \
+     dalvik.vm.heapminfree=512k \
+     dalvik.vm.heapmaxfree=8m
 
 
 PRODUCT_NAME := $(VENDOR_QTI_DEVICE)
