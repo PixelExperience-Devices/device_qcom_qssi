9930a2fbbb265ddfe9e5b5004fc48db403f76220
Siddeswar Aluganti <salugant@codeaurora.org>
2021-09-08 12:08:41 -0700

    Update SHIPPING_API_LEVEL
    1. Bump up SHIPPING_API_LEVEL to 31
    
    Change-Id: I9aaad6ae48b2adfe72095fa79dc476e6e04d82b5

--- qssi.mk
+++ qssi.mk
@@ -41,7 +41,7 @@ BOARD_AVB_ENABLE := true
 
 # Retain the earlier default behavior i.e. ota config (dynamic partition was disabled if not set explicitly), so set
 # SHIPPING_API_LEVEL to 28 if it was not set earlier (this is generally set earlier via build.sh per-target)
-SHIPPING_API_LEVEL := 30
+SHIPPING_API_LEVEL := 31
 
 $(call inherit-product-if-exists, vendor/qcom/defs/product-defs/system/cne_url*.mk)
 
